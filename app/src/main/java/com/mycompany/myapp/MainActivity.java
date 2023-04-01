ef42e8615d37cf9590ff3752fe55fb4d90ce07cc
GomdimApps <81498379+GomdimApps@users.noreply.github.com>
2023-04-01 16:54:57 -0300

    AIDE

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -1,15 +1,46 @@
 package com.mycompany.myapp;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.R;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+	DrawerLayout drawer;
+    androidx.appcompat.widget.Toolbar foodpanda;
+    NavigationView navbar;
+    ActionBarDrawerToggle hamburger;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
-    
+		drawer = (DrawerLayout) findViewById(R.id.drawer);
+        foodpanda = (androidx.appcompat.widget.Toolbar) findViewById(R.id.foodpanda);
+        navbar = (NavigationView) findViewById(R.id.navbar);
+        hamburger = new ActionBarDrawerToggle(this, drawer, foodpanda, R.string.open, R.string.close);
+        drawer.addDrawerListener(hamburger);
+        hamburger.syncState();
+        // configurações do Navigation menu
+        navbar.setNavigationItemSelectedListener(
+			new NavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(MenuItem p1) {
+					switch (p1.getItemId()) {
+						case R.id.item:
+							break; }
+					return false;
+				}
+			}
+        );
+	
+	}
+	
 }
+
